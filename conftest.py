0e8c35fbfefd86004ccf4680d4820393bf724df9
Raihan Khan <typetork@gmail.com>
2025-01-13 23:06:38 +0600

    remove headless

--- conftest.py
+++ conftest.py
@@ -4,5 +4,10 @@ import pytest
 def browser():
     # Setup and teardown for the browser session
     driver = setup_browser()
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # options.add_argument('--no-sandbox')
+    # driver = webdriver.Chrome(options=options)
+
     yield driver
     teardown_browser(driver)
